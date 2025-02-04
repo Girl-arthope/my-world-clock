ebf00985dcc2b262222feea3b1cf6e713479fa5b
tariro <chidemetariro@gmail.com>
2025-02-04 11:47:23 +0200

    added select for current location

--- javascript/index.js
+++ javascript/index.js
@@ -26,6 +26,10 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.split("/")[1].replace("_", " ");
 
   let cityTime = moment().tz(cityTimeZone);
